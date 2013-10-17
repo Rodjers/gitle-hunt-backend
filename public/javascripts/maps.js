9e8acf16626e0993d6b92b86abfeac3a104e26e4
Oddgeir Gitlestad <oddgeir.gitlestad@gitlestadit.no>
2013-10-17 19:19:37 +0200

    Observations now show up in the map

--- public/javascripts/maps.js
+++ public/javascripts/maps.js
@@ -5,19 +5,44 @@
  * Time: 9:20 PM
  * To change this template use File | Settings | File Templates.
  */
+var map;
 function initialize() {
+
     var LatLong = new google.maps.LatLng(60.25, 7.5);
     var mapOptions = {
         center: LatLong,
-        zoom: 13,
+        zoom: 6,
         mapTypeId: google.maps.MapTypeId.ROADMAP
     };
-    var map = new google.maps.Map(document.getElementById("map-canvas"),
+    map = new google.maps.Map(document.getElementById("map-canvas"),
         mapOptions);
-    var markerOptions = {
-        map: map,
-        position: LatLong
+    google.maps.event.addListener(map, 'tilesloaded', function() {
+        var north = map.getBounds().getNorthEast().lat();
+        var west = map.getBounds().getSouthWest().lng();
+        var south = map.getBounds().getSouthWest().lat();
+        var east = map.getBounds().getNorthEast().lng();
+    $.getJSON( "square?north=" + north + "&south=" + south + "&west=" + west + "&east=" + east, function( data ) {
+        drawCollection(data, map);
+    });
+});
+
+
+}
+function drawCollection(observations, map){
+
+    var i = 0;
+
+    while(observations[i] != undefined){
+        marker = new google.maps.Marker({
+            position: new google.maps.LatLng(observations[i].latitude, observations[i].longitude),
+            map: map
+
+        });
+        i++;
     }
-    var marker = new google.maps.Marker(markerOptions);
+
 }
 google.maps.event.addDomListener(window, 'load', initialize);
+
+
+
